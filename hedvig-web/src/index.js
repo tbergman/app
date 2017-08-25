5429349364b2757bd844be6051c4bea05c6ed7a8
Fredrik Stockman <fredrik@threadsafestudio.com>
2017-08-25 15:26:40 +0200

    Global styling

--- hedvig-web/src/index.js
+++ hedvig-web/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom"
 
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
+require("normalize.css")
+require("./index.css")
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
