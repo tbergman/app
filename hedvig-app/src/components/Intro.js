1ef638844d027c0f7ff771029a68850e5e3a400a
Fredrik Stockman <fredrik@threadsafestudio.com>
2017-08-28 14:46:48 +0200

    Started using rn-placeholder

--- hedvig-app/src/components/Intro.js
+++ hedvig-app/src/components/Intro.js
@@ -1,19 +1,50 @@
 import React from "react"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import Link from "../containers/Link"
-import { Placeholder } from "./Styles"
+import { BaseViewStyle } from "./Styles"
+import Placeholder from "rn-placeholder"
 
 export default class Intro extends React.Component {
   static navigationOptions = {
     title: "Intro"
   }
 
+  constructor() {
+    super()
+    this.state = {
+      ready: false
+    }
+  }
+
+  wait() {
+    setTimeout(() => {
+      this.setState({
+        ready: true
+      })
+    }, 2000)
+  }
+
   render() {
+    this.wait()
     return (
-      <Placeholder>
+      <BaseViewStyle>
         <Text>Intro</Text>
-        <Link to="Login" title="Login" />
-      </Placeholder>
+
+        <Placeholder.ImageContent
+          position="right"
+          hasRadius
+          lineNumber={5}
+          textSize={14}
+          lineSpacing={5}
+          color="#00ff00"
+          width="100%"
+          lastLineWidth="30%"
+          firstLineWidth="10%"
+          onReady={this.state.ready}
+        >
+          <Link to="Login" title="Login" />
+        </Placeholder.ImageContent>
+      </BaseViewStyle>
     )
   }
 }
