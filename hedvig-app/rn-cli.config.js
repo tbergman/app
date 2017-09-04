7e37a181006ea88e176e6c4a307acbf58e734a72
Fredrik Stockman <fredrik@threadsafestudio.com>
2017-09-04 14:19:28 +0200

    Upgraded to Expo 20

--- hedvig-app/rn-cli.config.js
+++ hedvig-app/rn-cli.config.js
@@ -1,14 +1,7 @@
 const path = require("path")
-const blacklist = require("metro-bundler/build/blacklist")
 
 module.exports = {
   getProjectRoots() {
     return [path.join(__dirname, ".."), __dirname]
-  },
-  getBlacklistRE() {
-    // prettier-ignore
-    return blacklist([
-      /hedvig-web\/.*/,
-    ])
   }
 }
