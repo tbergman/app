90546b4ed15f0d7ff51107e6501450baea72776f
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2017-10-20 11:49:22 +0100

    Bumped status message delay time

--- hedvig-redux/src/sagas/statusMessage.js
+++ hedvig-redux/src/sagas/statusMessage.js
@@ -8,7 +8,7 @@ import { take, takeEvery, put, select } from "redux-saga/effects"
 
 const delayHideStatusMessage = function*({ payload: { message, warning, error } }) {
   if (message || warning || error) {
-    let displayTime = 3000
+    let displayTime = 10000
     console.log(`Displaying status "${message || warning || error}" message for ${displayTime} ms`)
     yield delay(displayTime)
     yield put(statusMessageActions.setStatusMessage({}))
