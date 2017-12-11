302a44c6a7febbf75d17c5691a1770d1cebaf9af
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2017-12-11 17:15:22 +0100

    Added drop shadow to perils category

--- hedvig-app/src/components/styles/dashboard.js
+++ hedvig-app/src/components/styles/dashboard.js
@@ -39,6 +39,11 @@ export const StyledCategoryContainer = styled.TouchableOpacity`
   flex: 1;
   background-color: ${props => props.theme.colors.white};
   padding-top: 16px;
+  ${"" /* Drop shadow */} shadow-opacity: 0.1;
+  shadow-radius: 3;
+  shadow-offset: 0px 0px;
+  shadow-color: black;
+  elevation: 1;
 `
 
 export const StyledCategoryHeader = styled.View`
