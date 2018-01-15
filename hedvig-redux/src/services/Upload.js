e30cccb175171d9c2bcb55fc6efdcd08bd35ef2d
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-15 09:26:32 +0100

    Accept all upload statuses for now

--- hedvig-redux/src/services/Upload.js
+++ hedvig-redux/src/services/Upload.js
@@ -4,7 +4,9 @@ export function upload(url, opts = {}, onProgress) {
     var xhr = new XMLHttpRequest()
     xhr.open(opts.method || "get", url)
     for (var k in opts.headers || {}) xhr.setRequestHeader(k, opts.headers[k])
-    xhr.onload = result => result.target.status < 300 ? resolve(result) : reject(result)
+    // xhr.onload = result => result.target.status < 300 ? resolve(result) : reject(result)
+    // Accept all upload statuses for now
+    xhr.onload = resolve
     xhr.onerror = reject
     if (xhr.upload && onProgress) xhr.upload.onprogress = onProgress // event.loaded / event.total * 100 ; //event.lengthComputable
     xhr.send(opts.body)
