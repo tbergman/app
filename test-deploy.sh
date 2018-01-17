febe80cc199620e62e970a1fd7d45660220fec4b
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-17 12:52:44 +0100

    New attempt to make new deploy strategy work

---
+++ test-deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
+sudo sysctl fs.inotify.max_user_watches=524288
+sudo sysctl fs.inotify.max_queued_events=524288
+sudo sysctl -p
+yarn deploy
+exp logout
