30558a4a072d86d7930379d98cf98afd95b961cd
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-17 14:30:35 +0100

    Once more but for real this time

--- hedvig-app/test-deploy.sh
+++ hedvig-app/test-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -z "${TRAVIS_PULL_REQUEST}"] || [ "${TRAVIS_PULL_REQUEST}" == "false"]; then
+if [ -z "${TRAVIS_PULL_REQUEST}"] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     if [ "${TRAVIS_BRANCH}" == "master" ]; then
         exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
         sudo sysctl fs.inotify.max_user_watches=524288
