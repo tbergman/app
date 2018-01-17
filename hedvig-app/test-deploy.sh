8a3df31040b5cba4718b7baaf79449eed84d41b7
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-17 14:21:32 +0100

    Come on travis, pls work already

---
+++ hedvig-app/test-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [ -z "${TRAVIS_PULL_REQUEST}"] || [ "${TRAVIS_PULL_REQUEST}" == "false"]; then
+    if [ "${TRAVIS_BRANCH}" == "master" ]; then
+        exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
+        sudo sysctl fs.inotify.max_user_watches=524288
+        sudo sysctl fs.inotify.max_queued_events=524288
+        sudo sysctl -p
+        yarn deploy
+        exp logout
+    else
+        echo "Not on master, will not deploy to test"
+    fi
+else
+    echo "Not on master, will not deploy to test"
+fi
--- test-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-cd hedvig-app
-exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
-sudo sysctl fs.inotify.max_user_watches=524288
-sudo sysctl fs.inotify.max_queued_events=524288
-sudo sysctl -p
-yarn deploy
-exp logout
