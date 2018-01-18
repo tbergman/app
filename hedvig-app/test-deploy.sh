606ccb990626fb23e847ff278af59c5c0287b637
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-18 16:15:36 +0100

    Come on travis, work with me here

--- hedvig-app/test-deploy.sh
+++ hedvig-app/test-deploy.sh
@@ -4,10 +4,10 @@ if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; t
         sudo sysctl fs.inotify.max_user_watches=524288
         sudo sysctl fs.inotify.max_queued_events=524288
         sudo sysctl -p
-        yarn deploy
+        exp publish
         sed -i -e 's/gateway.test.hedvig.com/hedvig.ngrok.io/g' ../hedvig-redux/src/services/environment.js
         sed -i -e 's/"slug": "hedvig-app"/"slug": "hedvig-ngrok-app"/g' app.json
-        yarn deploy
+        exp publish
         exp logout
     else
         echo "Not on master, will not deploy to test"
