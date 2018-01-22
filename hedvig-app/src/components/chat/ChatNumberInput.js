ec512a876e6c02b4e0785f818d9b6ecfaca547da
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-22 15:46:47 +0100

    Fixed double presses creating multiple chat actions

--- hedvig-app/src/components/chat/ChatNumberInput.js
+++ hedvig-app/src/components/chat/ChatNumberInput.js
@@ -2,25 +2,37 @@ import React from "react"
 import { StyledTextInputContainer, StyledTextInput } from "../styles/chat"
 import { SendIconButton, SendDisabledIconButton } from "../Button"
 
-const ChatTextInput = ({ message, onChange, send }) => {
-  let ButtonComponent =
-    message._inputValue && message._inputValue.length > 0
-      ? SendIconButton
-      : SendDisabledIconButton
-  return (
-    <StyledTextInputContainer>
-      <StyledTextInput
-        placeholder="Type here..."
-        autoFocus={true}
-        keyboardType="numeric"
-        value={message._inputValue || ""}
-        underlineColorAndroid="transparent"
-        onChangeText={text => onChange(message, text)}
-        onSubmitEditing={() => send(message)}
-      />
-      <ButtonComponent onPress={() => send(message)} />
-    </StyledTextInputContainer>
-  )
+class ChatTextInput extends React.Component {
+  buttonPressed = false
+
+  _send = () => {
+    if (!this.buttonPressed) {
+      this.buttonPressed = true
+      this.props.send(this.props.message)
+    }
+  }
+
+  render() {
+    const { message, onChange } = this.props
+    let ButtonComponent =
+      message._inputValue && message._inputValue.length > 0
+        ? SendIconButton
+        : SendDisabledIconButton
+    return (
+      <StyledTextInputContainer>
+        <StyledTextInput
+          placeholder="Type here..."
+          autoFocus={true}
+          keyboardType="numeric"
+          value={message._inputValue || ""}
+          underlineColorAndroid="transparent"
+          onChangeText={text => onChange(message, text)}
+          onSubmitEditing={this._send}
+        />
+        <ButtonComponent onPress={this._send} />
+      </StyledTextInputContainer>
+    )
+  }
 }
 
 export default ChatTextInput
