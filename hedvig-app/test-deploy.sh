0d793f5320e524556056f320e0aa6bf98af4c332
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-26 13:45:05 +0100

    Moved things around (come on travis, work with me)

--- hedvig-app/test-deploy.sh
+++ hedvig-app/test-deploy.sh
@@ -1,12 +1,13 @@
 if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     if [ "${TRAVIS_BRANCH}" == "master" ]; then
-        exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
         sudo sysctl fs.inotify.max_user_watches=524288
         sudo sysctl fs.inotify.max_queued_events=524288
         sudo sysctl -p
+        exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
         exp publish
         sed -i -e 's/gateway.test.hedvig.com/hedvig.ngrok.io/g' ../hedvig-redux/src/services/environment.js
         sed -i -e 's/"slug": "hedvig-app"/"slug": "hedvig-ngrok-app"/g' app.json
+        exp login -u ${EXPO_DEV_USERNAME} -p ${EXPO_DEV_PASSWORD}
         exp publish
         exp logout
     else
