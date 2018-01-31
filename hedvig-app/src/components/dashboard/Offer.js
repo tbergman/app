0c6537dd9572bf416702161402a5f981903afbda
Oscar Nylander <oscar.nylander@etimo.se>
2018-01-31 15:18:22 +0100

    PUL clickable

--- hedvig-app/src/components/dashboard/Offer.js
+++ hedvig-app/src/components/dashboard/Offer.js
@@ -92,7 +92,7 @@ class Offer extends React.Component {
             ))}
           </View>
           <OfferFooter />
-          <StyledPassiveText style={{paddingLeft: 24, paddingRight: 18, paddingBottom: 135}}>
+          <StyledPassiveText style={{paddingLeft: 24, paddingRight: 18, paddingBottom: 160}}>
             Genom att trycka bli medlem bekräftar jag att jag tagit del av&nbsp;
             <TextLink to={this.props.insurance.presaleInformationUrl}>förköpsinformation</TextLink>,
             Hedvigs <TextLink to={this.props.insurance.policyUrl}>försäkringsvillkor</TextLink> och att mina personuppgifter&nbsp;
