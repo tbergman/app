aa4652493329000d90bb3c3a8e9e57db40e73046
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-06 16:09:10 +0100

    Clean upp and update some dependencies

--- hedvig-app/src/components/Placeholder.js
+++ hedvig-app/src/components/Placeholder.js
@@ -1,22 +1,5 @@
 import React from "react"
-import Placeholder from "rn-placeholder"
 import { TextplainerStyle } from "./Styles"
-
-export const ImageContentPlaceholder = () => {
-  return (
-    <Placeholder.ImageContent
-      position="left"
-      hasRadius
-      lineNumber={5}
-      textSize={14}
-      lineSpacing={5}
-      color="gainsboro"
-      width="100%"
-      lastLineWidth="30%"
-      firstLineWidth="10%"
-    />
-  )
-}
 
 export const Textplainer = ({ text }) => {
   return (
