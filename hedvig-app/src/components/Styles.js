74cce9073f9349d3cfefedcced956f71b3c989f4
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-07 11:23:43 +0100

    More cleanup

--- hedvig-app/src/components/Styles.js
+++ hedvig-app/src/components/Styles.js
@@ -9,14 +9,3 @@ export const BaseScrollViewStyle = styled.ScrollView`
   flex: 1;
   background-color: white;
 `
-
-export const TextplainerStyle = styled.Text`
-  border-style: solid;
-  border-width: 1px;
-  border-color: black;
-  padding: 40px 20px;
-  font-size: 30px;
-  align-self: stretch;
-  text-align: center;
-  margin: 20px;
-`
