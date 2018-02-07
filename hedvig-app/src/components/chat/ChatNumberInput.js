aeb780e6b31118e6f65d2f8fd117ac4632050cae
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-07 16:35:18 +0100

    la deuda

--- hedvig-app/src/components/chat/ChatNumberInput.js
+++ hedvig-app/src/components/chat/ChatNumberInput.js
@@ -1,8 +1,15 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { StyledTextInputContainer, StyledTextInput } from "../styles/chat"
 import { SendIconButton, SendDisabledIconButton } from "../Button"
 
 class ChatTextInput extends React.Component {
+  static propTypes = {
+    message: PropTypes.object, // TODO Better definition of message type
+    onChange: PropTypes.func.isRequired,
+    send: PropTypes.func.isRequired
+  }
+
   lastSentFor = undefined
 
   _send = () => {
