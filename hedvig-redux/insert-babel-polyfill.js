8cabd9cf38e7e2bd8a2c625d7bba15b27df03526
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-14 14:00:51 +0100

    New strategy for handling babel polyfill

--- hedvig-redux/insert-babel-polyfill.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs")
-
-let input = fs.readFileSync("./src/index.js")
-let output = `import "babel-polyfill"\n\n${input}`
-fs.writeFile("./src/index.js", output, function(err) {
-  if (err) {
-    console.error(err)
-  }
-})
