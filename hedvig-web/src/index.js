171022a0451cd976d1b818bb22d5977e3bc89bd5
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-14 14:01:26 +0100

    Removed service worker (we dont use it)

--- hedvig-web/src/index.js
+++ hedvig-web/src/index.js
@@ -3,12 +3,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import App from "./App"
-import registerServiceWorker from "./registerServiceWorker"
+// import registerServiceWorker from "./registerServiceWorker"
 require("normalize.css")
 require("./index.css")
 
 ReactDOM.render(<App />, document.getElementById("root"))
-registerServiceWorker()
+// registerServiceWorker()
 
 if (module.hot) {
     module.hot.accept("./App", () => {
