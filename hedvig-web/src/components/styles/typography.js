2e7a0831a7a19231edd6affb91e24a59b7bddefe
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-15 16:26:48 +0100

    Cleanup

--- hedvig-web/src/components/styles/typography.js
+++ hedvig-web/src/components/styles/typography.js
@@ -1,17 +1,5 @@
 import styled from "styled-components"
 
-export const Heading1 = styled.span`
-  font-family: "Merriweather";
-  color: ${props => props.theme.colors.webHeading};
-  font-size: 28px;
-  line-height: 38px;
-
-  @media screen and (min-width: 800px) {
-    font-size: 40px;
-    line-height: 56px;
-  }
-`
-
 export const Heading2 = styled.span`
   font-family: "Merriweather";
   color: ${props => props.theme.colors.webHeading};
@@ -22,19 +10,4 @@ export const Heading2 = styled.span`
     font-size: 40px;
     line-height: 56px;
   }
-`
-
-export const WhiteHeading2 = Heading2.extend`
-  color: white;
-  font-size: 28px;
-  line-height: 38px;
-
-  @media (min-width: 800px) {
-    font-size: 40px;
-    line-height: 56px;
-  }
-`
-
-export const BlackPurpleHeading2 = Heading2.extend`
-  color: ${props => props.theme.colors.blackPurple};
 `
