d8473ac37bde81f2ac6c4b8bcbdd9fc4e3169f7b
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-07 11:34:28 +0100

    Removed the mail option for adding a receipt in AddEditAsset

--- hedvig-app/src/components/asset-tracker/AddEditAsset.js
+++ hedvig-app/src/components/asset-tracker/AddEditAsset.js
@@ -380,33 +380,13 @@ export default class AddEditAsset extends React.Component {
       item.receiptUrl = result.uri
       this.setState({ item })
     }
-    let actionSheetOptions = [
-      {
-        label: "Fota kvitto",
-        onSelected: () => this._takePhoto(onPhotoPickedOrTaken)
-      },
-      {
-        label: "Maila kvitto",
-        onSelected: () => this.props.showDialog({
-          title: "Maila in kvitto",
-          paragraph: "Maila dina kvitton till kvitton@hedvig.com så lägger Hedvig till informationen automatiskt",
-          confirmButtonTitle: "Kopiera Mailadressen",
-          dismissButtonTitle: "Avbryt",
-          onConfirm: () => {
-            Clipboard.setString("kvitton@hedvig.com");
-            this.props.setStatusMessage("kvitton@hedvig.com kopierat")
-          },
-          onDismiss: () => {}
-        })
-      }
-    ]
     let message = this.state.item.receiptUrl
       ? "Kvitto tillagt"
-      : "Lägg till genom att fota eller maila in kvittot"
+      : "Fota kvittot för prylen"
     return (
       <StyledInputContainer
         activeOpacity={1}
-        onPress={() => this._showActionSheet(actionSheetOptions)}
+        onPress={() => this._takePhoto(onPhotoPickedOrTaken)}
       >
         <StyledInputTexts>
           <StyledInputHeader>Kvitto</StyledInputHeader>
