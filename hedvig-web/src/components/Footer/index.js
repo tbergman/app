cda18e861320b0fc811ca6ca7cbefbcba19cf383
Philip Harrison <philip@mailharrison.com>
2018-03-20 20:41:24 +0100

    Add app download page

--- hedvig-web/src/components/Footer/index.js
+++ hedvig-web/src/components/Footer/index.js
@@ -16,54 +16,10 @@ const Footer = () => (
             Kontakt
           </Link>
           <Link className="Footer__link" to="/legal">
-            Legal information
+            Legal&nbsp;information
           </Link>
         </nav>
       </div>
-    </div>
-    <div className="pure-u-1-1 pure-u-md-2-3 Footer__social">
-      {/*<a href="https://www.instagram.com/hedvigers" target="_blank" rel="noopener noreferrer">
-        <img
-          src="/assets/web/Social icons/Instagram.svg"
-          alt="Instagram"
-          width={40}
-          height={54}
-        />
-      </a>*/}
-      <a
-        href="https://itunes.apple.com/se/app/hedvig/id1303668531?mt=8"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="Footer__social-icon"
-      >
-        <Lazyload
-          height={46}
-          offset={200}
-        >
-          <img
-            src="/assets/web/appstores/app-store-badge@2x.png"
-            alt="Ladda ner på App Store"
-            height={54}
-          />
-        </Lazyload>
-      </a>
-      <a
-        href="https://play.google.com/store/apps/details?id=com.hedvig.app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="Footer__social-icon"
-      >
-        <Lazyload
-          height={46}
-          offset={200}
-        >
-          <img
-            src="/assets/web/appstores/google-play-badge@2x.png"
-            alt="Ladda ner på Google Play"
-            height={54}
-          />
-        </Lazyload>
-      </a>
     </div>
   </footer>
 )
