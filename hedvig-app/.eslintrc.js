c568888b48286834bc39df635aa9f7240da7ebd1
Philip Harrison <philip@mailharrison.com>
2018-04-04 14:33:29 +0200

    Update gitignore, prettier editor config
    Matching https://github.com/HedvigInsurance/webs

---
+++ hedvig-app/.eslintrc.js
@@ -0,0 +1,27 @@
+const OFF = 'off';
+const ERROR = 'error';
+const WARN = 'warn';
+
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    jest: true
+  },
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2016,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'no-undef': ERROR,
+    'no-unused-vars': WARN,
+    'react/prop-types': OFF,
+    'react/display-name': OFF,
+    'no-debugger': OFF
+  }
+}
