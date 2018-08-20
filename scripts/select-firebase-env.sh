ed64f3fb98abef0be78c3e7353bc0427fc76b541
Oscar Nylander <oscar@hedvig.com>
2018-08-20 11:20:18 +0200

    s/mv/cp

--- scripts/select-firebase-env.sh
+++ scripts/select-firebase-env.sh
@@ -4,16 +4,16 @@ set -uex
 
 case "${ENVIRONMENT}" in
     "development")
-        mv android/app/src/debug/google-services.json android/app/google-services.json;
-        mv ios/hedvig/GoogleService-Info.development.plist ios/hedvig/GoogleService-Info.plist ;;
+        cp android/app/src/debug/google-services.json android/app/google-services.json;
+        cp ios/hedvig/GoogleService-Info.development.plist ios/hedvig/GoogleService-Info.plist ;;
 
     "staging")
-        mv android/app/src/staging/google-services.json android/app/google-services.json;
-        mv ios/hedvig/GoogleService-Info.staging.plist ios/hedvig/GoogleService-Info.plist ;;
+        cp android/app/src/staging/google-services.json android/app/google-services.json;
+        cp ios/hedvig/GoogleService-Info.staging.plist ios/hedvig/GoogleService-Info.plist ;;
 
     "production")
-        mv android/app/src/release/google-services.json android/app/google-services.json;
-        mv ios/hedvig/GoogleService-Info.production.plist ios/hedvig/GoogleService-Info.plist ;;
+        cp android/app/src/release/google-services.json android/app/google-services.json;
+        cp ios/hedvig/GoogleService-Info.production.plist ios/hedvig/GoogleService-Info.plist ;;
 
     *)
         echo "ENVIRONMENT must be one of: DEVELOPMENT, STAGING, PRODUCTION"; exit 1 ;;
