71ceaf7ae389cf56f7c278e72469eefc7b51d1b0
Sam Pettersson <sam@sampettersson.com>
2018-08-31 10:55:28 +0200

    Add qoutes around env variable

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -5,4 +5,4 @@ PLIST_FILE="ios/hedvig/Supporting/Info.plist"
 
 echo "**************** PUBLISH CHANGES WITH CODEPUSH ******************"
 
-appcenter codepush release-react -a "$APP_CENTER_APP_NAME" -m --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" --token "$APP_CENTER_TOKEN" --plist-file $PLIST_FILE
+appcenter codepush release-react -a "$APP_CENTER_APP_NAME" -m --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" --token "$APP_CENTER_TOKEN" --plist-file "$PLIST_FILE"
