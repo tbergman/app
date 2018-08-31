c8d32eaa6f253d2c4648245ad33269a90248374a
Sam Pettersson <sam@sampettersson.com>
2018-08-31 14:11:49 +0200

    Try this then

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,7 +7,10 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
+ARCHIVE_DIR="$APPCENTER_OUTPUT_DIRECTORY/archive/Staging.xcarchive/Products/Applications/Hedvig.app"
+
 echo "**************** PUBLISH CHANGES WITH CODEPUSH ******************"
 echo "$APPCENTER_OUTPUT_DIRECTORY"
+echo "$ARCHIVE_DIR"
 
-appcenter codepush release -a "$APP_CENTER_APP_NAME" -c "$APPCENTER_OUTPUT_DIRECTORY" --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" -t "$PACKAGE_VERSION" --token "$APP_CENTER_TOKEN"
+appcenter codepush release -a "$APP_CENTER_APP_NAME" -c "$ARCHIVE_DIR" --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" -t "$PACKAGE_VERSION" --token "$APP_CENTER_TOKEN"
