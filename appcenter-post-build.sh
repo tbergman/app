89f8839301cc7cafc1608264003d8bbf1fb25cc5
Sam Pettersson <sam@sampettersson.com>
2018-08-31 14:56:21 +0200

    Add BUNDLE_NAME

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -8,8 +8,10 @@ IOS_ARCHIVE_DIR="$APPCENTER_OUTPUT_DIRECTORY/archive/Staging.xcarchive/Products/
 
 if [ "$PLATFORM" = "ANDROID" ]; then
     ARCHIVE_DIR="$ANDROID_ARCHIVE_DIR"
+    BUNDLE_NAME="index.android.bundle"
 else
     ARCHIVE_DIR="$IOS_ARCHIVE_DIR"
+    BUNDLE_NAME="main.jsbundle"
 fi
 
 TMP_FOLDER="/tmp/codepush"
@@ -19,7 +21,7 @@ echo "$APPCENTER_OUTPUT_DIRECTORY"
 echo "$ARCHIVE_DIR"
 
 mkdir "$TMP_FOLDER"
-cp "$ARCHIVE_DIR/main.jsbundle" "$TMP_FOLDER"
+cp "$ARCHIVE_DIR/$BUNDLE_NAME" "$TMP_FOLDER"
 cp -r "$ARCHIVE_DIR/assets" "$TMP_FOLDER"
 
 appcenter codepush release -a "$APP_CENTER_APP_NAME" -c "$TMP_FOLDER" --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" -t "$PACKAGE_VERSION" --token "$APP_CENTER_TOKEN"
