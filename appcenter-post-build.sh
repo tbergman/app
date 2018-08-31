afc06207ecabbf488909c0d77430aeefd4bd0c9e
Sam Pettersson <sam@sampettersson.com>
2018-08-31 10:09:12 +0200

    Tweak build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -6,4 +6,4 @@ echo "**************** PUBLISH CHANGES WITH CODEPUSH ******************"
 echo "APP NAME                => $APP_NAME"
 echo "SELECTED RN PACKAGE     => $APPCENTER_REACTNATIVE_PACKAGE"
 
-appcenter codepush release-react -a "$APP_NAME" -m --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_URL" --token "$APP_CENTER_TOKEN"
+appcenter codepush release-react -a "$APP_NAME" -m --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" --token "$APP_CENTER_TOKEN"
