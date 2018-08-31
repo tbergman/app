c96d34a5e6e740117e44104edfc0f4d45f42bb43
Sam Pettersson <sam@sampettersson.com>
2018-08-31 15:18:29 +0200

    Testing

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -24,4 +24,4 @@ mkdir "$TMP_FOLDER"
 cp "$ARCHIVE_DIR/$BUNDLE_NAME" "$TMP_FOLDER"
 cp -r "$ARCHIVE_DIR/assets" "$TMP_FOLDER"
 
-appcenter codepush release -a "$APP_CENTER_APP_NAME" -c "$TMP_FOLDER" --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" -t "$PACKAGE_VERSION" --token "$APP_CENTER_TOKEN"
+appcenter codepush release -a "$APP_CENTER_APP_NAME" -c "$TMP_FOLDER" --description "hello" -d "$CODE_PUSH_DEPLOYMENT_NAME" -t "$PACKAGE_VERSION" --token "$APP_CENTER_TOKEN"
