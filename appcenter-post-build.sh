ee5b282634fd05ba9c14f5922a6db9cc7a720de4
Sam Pettersson <sam@sampettersson.com>
2018-08-31 10:28:28 +0200

    Run appcenter release from source dir

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,7 +3,5 @@
 COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 
 echo "**************** PUBLISH CHANGES WITH CODEPUSH ******************"
-echo "APP NAME                => $APP_NAME"
-echo "SELECTED RN PACKAGE     => $APPCENTER_REACTNATIVE_PACKAGE"
 
-appcenter codepush release-react -a "$APP_NAME" -m --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" --token "$APP_CENTER_TOKEN"
+cd $APPCENTER_SOURCE_DIRECTORY; appcenter codepush release-react -a "$APP_CENTER_APP_NAME" -m --description "$COMMIT_MESSAGE" -d "$CODE_PUSH_DEPLOYMENT_NAME" --token "$APP_CENTER_TOKEN"
