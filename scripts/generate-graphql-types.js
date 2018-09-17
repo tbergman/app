434a43c950a67aaf90f3460819f650707e1d90e0
Oscar Nylander <oscar@hedvig.com>
2018-09-17 12:35:42 +0200

    Make typedefs available for type generation script

--- scripts/generate-graphql-types.js
+++ scripts/generate-graphql-types.js
@@ -1,66 +1,7 @@
 /* eslint-disable no-console */
 const graphqlSchemaTypescript = require('graphql-schema-typescript');
 const { makeExecutableSchema } = require('graphql-tools');
-
-const typeDefs = `
-type Insurance {
-  address: String
-  monthlyCost: Int
-  safetyIncreasers: [String!]
-  certificateUrl: String
-  status: InsuranceStatus
-  type: InsuranceType
-  activeFrom: LocalDate
-
-  perilCategories: [PerilCategory]
-}
-
-type PerilCategory {
-  title: String
-  description: String
-  iconUrl: String
-  perils: [Peril]
-}
-
-type Peril {
-  id: ID
-  title: String
-  imageUrl: String
-  description: String
-}
-
-enum InsuranceStatus {
-  PENDING
-  ACTIVE
-  INACTIVE
-  INACTIVE_WITH_START_DATE
-  TERMINATED
-}
-
-enum InsuranceType {
-  RENT
-  BRF
-  STUDENT_RENT
-  STUDENT_BRF
-}
-
-type Cashback {
-  id: ID
-  name: String
-  imageUrl: String
-}
-
-type Query {
-  insurance: Insurance!
-  cashback: Cashback!
-}
-
-type Mutation {
-  logout: Boolean
-}
-
-scalar LocalDate
-`;
+const { typeDefs } = require('../src/graphql/typedefs');
 
 graphqlSchemaTypescript
   .generateTypeScriptTypes(
--- src/graphql/client.js
+++ src/graphql/client.js
@@ -4,70 +4,12 @@ import { SchemaLink } from 'apollo-link-schema';
 import { makeExecutableSchema } from 'graphql-tools';
 
 import { resolvers } from './resolvers';
-
-const typeDefs = `
-  type Insurance {
-    address: String
-    monthlyCost: Int
-    safetyIncreasers: [String!]
-    certificateUrl: String
-    status: InsuranceStatus
-    type: InsuranceType
-    activeFrom: LocalDate
-
-    perilCategories: [PerilCategory]
-  }
-
-  type PerilCategory {
-    title: String
-    description: String
-    iconUrl: String
-    perils: [Peril]
-  }
-
-  type Peril {
-    id: ID
-    title: String
-    imageUrl: String
-    description: String
-  }
-
-  enum InsuranceStatus {
-    PENDING
-    ACTIVE
-    INACTIVE
-    INACTIVE_WITH_START_DATE
-    TERMINATED
-  }
-
-  enum InsuranceType {
-    RENT
-    BRF
-    STUDENT_RENT
-    STUDENT_BRF
-  }
-
-  type Cashback {
-    id: ID
-    name: String
-    imageUrl: String
-  }
-
-  type Query {
-    insurance: Insurance!
-    cashback: Cashback!
-  }
-
-  type Mutation {
-    logout: Boolean
-  }
-
-  scalar LocalDate
-`;
+import { typeDefs } from './typedefs';
+import { getToken, deleteToken } from './context';
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new SchemaLink({ schema }),
+  link: new SchemaLink({ schema, context: { getToken, deleteToken } }),
 });
