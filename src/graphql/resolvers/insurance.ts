77e0d4ff2902af210806d4c7ccddcb5408341512
Oscar Nylander <oscar@hedvig.com>
2018-09-23 15:12:33 +0200

    Remove some logs that I forgot to remove prior to merging apollo

--- src/graphql/resolvers/insurance.ts
+++ src/graphql/resolvers/insurance.ts
@@ -7,14 +7,12 @@ const getInsurance = async (token) => {
   const data = await fetch(`${Config.API_BASE_URL}/insurance`, {
     headers: { Authorization: `Bearer ${token}` },
   });
-  console.log('got insurance')
   return data.json();
 };
 
 const insurance: QueryToInsuranceResolver<Query, Insurance> = async (_root, _args, { getToken }) => {
   const token = await getToken()
   const [insurance, user] = await Promise.all([getInsurance(token), getUser(token)]);
-  console.log('getting insurance')
   return {
     monthlyCost: insurance.currentTotalPrice,
     address: insurance.addressStreet,
--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -4,7 +4,6 @@ const getUser = async (token) => {
   const data = await fetch(`${Config.API_BASE_URL}/member/me`, {
     headers: { Authorization: `Bearer ${token}` },
   });
-  console.log('got user')
   return data.json();
 };
 
