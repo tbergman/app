d31e179459feeb33ab094a9b4d59841f3ee60450
Sam Pettersson <sam@sampettersson.com>
2018-09-25 10:59:45 +0200

    Add support for placeholders

--- src/components/translations/consumer.tsx
+++ src/components/translations/consumer.tsx
@@ -4,15 +4,36 @@ import { TranslationsContext } from './context';
 interface ConsumerProps {
   children: (text: string) => React.ReactNode;
   textKey: string;
+  placeholders?: string[];
 }
+
+const placeholderRegex = new RegExp('{([0-9]+)}', 'g');
+const placeholderIndexRegex = new RegExp('([0-9]+)', 'g');
+
+export const replacePlaceholders = (placeholders: string[], text: string) =>
+  text.replace(placeholderRegex, (placeholder) => {
+    if (!placeholderIndexRegex.test(placeholder)) return placeholder;
+    const index = parseInt(placeholder.match(placeholderIndexRegex)![0]);
+
+    if (placeholders[index]) {
+      return placeholders[index];
+    }
+
+    return placeholder;
+  });
 
 export const TranslationsConsumer: React.SFC<ConsumerProps> = ({
   children,
   textKey,
+  placeholders,
 }) => (
   <TranslationsContext.Consumer>
     {({ textKeys }) => {
-      return children(textKeys[textKey] || textKey);
+      return children(
+        (placeholders
+          ? replacePlaceholders(placeholders, textKeys[textKey])
+          : textKeys[textKey]) || textKey,
+      );
     }}
   </TranslationsContext.Consumer>
 );
