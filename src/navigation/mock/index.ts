16315f9fe0e9a71d6bec533a5dd3b97780ecceab
Sam Pettersson <sam@sampettersson.com>
2018-10-03 16:03:33 +0200

    Try a different approach to launching mock screens

--- src/navigation/mock/index.ts
+++ src/navigation/mock/index.ts
@@ -1,8 +1,10 @@
+import { Navigation } from 'react-native-navigation';
 import { getNewOfferingScreensLayout } from 'src/navigation/mock/new-offering-screens';
 
-export const getMockLayout = (url: string) => {
+export const openMockLayout = async (url: string) => {
   if (url.includes('mock/new-offering-screens')) {
-    return getNewOfferingScreensLayout();
+    Navigation.setRoot(await getNewOfferingScreensLayout());
+    return;
   }
 
   throw new Error(`Tried to open faulty mock screen: ${url}`);
