3d1eea166a432f9fa95f29365e635ab99d0a27da
Sam Pettersson <sam@sampettersson.com>
2018-10-04 14:53:07 +0200

    Add TRACK_OFFER_OPENED to new offering screens

--- src/features/new-offer/components/checkout.tsx
+++ src/features/new-offer/components/checkout.tsx
@@ -3,9 +3,25 @@ import { connect } from 'react-redux';
 import { OFFER_CHECKOUT } from 'src/features/offer/state/actions';
 import { NavigationEvents } from 'src/navigation/events';
 import { Dialog } from 'src/features/bankid/Dialog';
+import { Mount } from 'react-lifecycle-components';
 
-export const CheckoutComp = ({ checkout }: { checkout: () => void }) => (
+import { TRACK_OFFER_OPENED } from 'src/features/analytics/actions';
+
+interface CheckoutProps {
+  monthlyCost: number;
+  orderId: number;
+  checkout: () => void;
+  trackOfferOpen: (monthlyCost: number, orderId: number) => void;
+}
+
+export const CheckoutComp: React.SFC<CheckoutProps> = ({
+  checkout,
+  orderId,
+  trackOfferOpen,
+  monthlyCost,
+}) => (
   <>
+    <Mount on={() => trackOfferOpen(monthlyCost, orderId)}>{null}</Mount>
     <NavigationEvents
       onGlobalEvent={(event: { id: string }) => {
         if (event.id === 'SignButtonPressed') {
@@ -24,10 +40,21 @@ const mapDispatchToProps = (dispatch: any) => {
         type: OFFER_CHECKOUT,
       });
     },
+    trackOfferOpen: (pricePerMonth: number, orderId: number) =>
+      dispatch({
+        type: TRACK_OFFER_OPENED,
+        payload: {
+          revenue: pricePerMonth,
+          currency: 'SEK',
+          order_id: orderId,
+        },
+      }),
   };
 };
 
 export const Checkout = connect(
-  null,
+  ({ analytics }: { analytics: { orderId: number } }) => ({
+    orderId: analytics.orderId,
+  }),
   mapDispatchToProps,
 )(CheckoutComp);
