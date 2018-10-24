7a02b17d5eb9f4b6e980ce5ef0dda246222cce50
Gustaf Gunér <gustaf@guner.se>
2018-10-24 17:40:09 +0200

    Removed old tests

--- src/features/chat/Chat.test.tsx
+++ src/features/chat/Chat.test.tsx
@@ -7,19 +7,4 @@ describe('<Chat />', () => {
   it('Should render without crashing', () => {
     expect(shallow(<Chat isModal={false} />)).not.toThrow();
   });
-
-  it('Should start polling when mounted', () => {
-    const spy = jest.spyOn(Chat.prototype, '_startPolling');
-    const wrapper = mount(<Chat isModal={false} />);
-    wrapper.instance();
-    expect(spy).toHaveBeenCalled();
-  });
-
-  it('Should stop polling when unmounted', () => {
-    const spy = jest.spyOn(Chat.prototype, '_stopPolling');
-    const wrapper = mount(<Chat isModal={false} />);
-    wrapper.instance();
-    wrapper.unmount();
-    expect(spy).toHaveBeenCalled();
-  });
 });
--- src/features/chat/OfferChat.test.tsx
+++ src/features/chat/OfferChat.test.tsx
@@ -7,19 +7,4 @@ describe('<OfferChat />', () => {
   it('Should render without crashing', () => {
     expect(shallow(<OfferChat />)).not.toThrow();
   });
-
-  it('Should start polling when mounted', () => {
-    const spy = jest.spyOn(OfferChat.prototype, '_startPolling');
-    const wrapper = mount(<OfferChat />);
-    wrapper.instance();
-    expect(spy).toHaveBeenCalled();
-  });
-
-  it('Should stop polling when unmounted', () => {
-    const spy = jest.spyOn(OfferChat.prototype, '_stopPolling');
-    const wrapper = mount(<OfferChat />);
-    wrapper.instance();
-    wrapper.unmount();
-    expect(spy).toHaveBeenCalled();
-  });
 });
