0d92bad59f9d202b164b1bdfdca979a3a5bbc98e
Sam Pettersson <sam@sampettersson.com>
2018-10-26 12:52:44 +0200

    Add date formatting to insurance activeFrom

--- src/features/dashboard/components/InsuranceStatus.tsx
+++ src/features/dashboard/components/InsuranceStatus.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, Image, Text } from 'react-native';
 import styled from '@sampettersson/primitives';
+import { format } from 'date-fns';
 
 import { fonts, colors } from '@hedviginsurance/brand';
 import { InsuranceStatus } from 'src/graphql/types';
@@ -48,7 +49,7 @@ const getStatusText = (status: InsuranceStatus, activeFrom: string) => {
     case 'ACTIVE':
       return 'Aktiv';
     case 'INACTIVE_WITH_START_DATE':
-      return `Aktiveras ${activeFrom}`;
+      return `Aktiveras ${format(activeFrom, 'YYYY-MM-DD')}`;
     case 'INACTIVE':
       return 'Inaktiv';
   }
